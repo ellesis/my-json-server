aa79dfeb5a83b34993c541f2bdd05f0ab703cb87
Lisa Han <ellesis@gmail.com>
2021-08-25 15:32:57 -0700

    fix server.js

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const path = require('path')
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const router = jsonServer.router('db.json') // <== Will be created later
 const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 3200 // <== You can change the port
 
-// Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
-
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
-server.use(jsonServer.bodyParser)
-
 server.use(router)
 
-let port = 4000
 server.listen(port, () => {
   console.log(`JSON Server is running, port(${port})`)
 })
